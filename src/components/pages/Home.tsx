1af4b29505f453c8652eae12a44d4b1f7a158c8d
asitis <asitisdev@gmail.com>
2025-05-10 04:29:08 +0900

    fix: 아이콘 변경

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router';
-import { Search, UserRound } from 'lucide-react';
+import { IconSearch, IconUserCircle } from '@tabler/icons-react';
 import HomeCarousel from '@/components/home-carousel';
 import ItemCarousel from '@/components/item-carousel';
 import RecipeCarousel from '@/components/recipe-story-carousel';
@@ -9,12 +9,12 @@ export default function Home() {
     <div className="flex flex-col gap-8 px-6 py-8">
       <header className="flex flex-row justify-between items-center px-2">
         <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">2025년 05월 09일</h1>
-        <div className="flex flex-row gap-5">
+        <div className="flex flex-row items-center gap-5">
           <Link to="/search">
-            <Search className="size-7" />
+            <IconSearch className="size-7" />
           </Link>
           <Link to="/mypage">
-            <UserRound className="size-7" />
+            <IconUserCircle className="size-8" />
           </Link>
         </div>
       </header>
