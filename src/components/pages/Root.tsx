f3a404dcf67d77bc048bcabff4ff91aaaa3638d4
asitis <asitisdev@gmail.com>
2025-05-10 04:11:19 +0900

    feat: 메뉴바 숨김 처리 구현

--- src/components/pages/Root.tsx
+++ src/components/pages/Root.tsx
@@ -1,16 +1,21 @@
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import MenuBar from '@/components/menu-bar';
 
 export default function Root() {
+  const location = useLocation();
+  const isMenubarVisible = !location.pathname.startsWith('/item') && !location.pathname.startsWith('/product');
+
   return (
     <>
       <main className="h-screen overflow-y-auto pb-16">
         <Outlet />
       </main>
 
-      <div className="fixed bottom-0 left-0 right-0 flex justify-center pb-4 z-50">
-        <MenuBar />
-      </div>
+      {isMenubarVisible && (
+        <div className="fixed bottom-0 left-0 right-0 flex justify-center pb-4 z-50">
+          <MenuBar />
+        </div>
+      )}
     </>
   );
 }
