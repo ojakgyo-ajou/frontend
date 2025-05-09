a7e09de907ecc781a9f3d0007f3b0c35192d2e8f
asitis <asitisdev@gmail.com>
2025-05-10 06:58:06 +0900

    feat: 홈화면 배너 이미지 추가

--- src/components/home-carousel.tsx
+++ src/components/home-carousel.tsx
@@ -1,8 +1,12 @@
+import Banner1 from '../assets/Banner1.png';
+import Banner2 from '../assets/Banner2.png';
+import Banner3 from '../assets/Banner3.png';
+
 export default function HomeCarousel() {
   return (
     <div className="carousel w-full">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp" className="w-full" />
+        <img src={Banner1} className="w-full object-cover" alt="Banner 1" />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a href="#slide4" className="btn btn-circle">
             ❮
@@ -13,7 +17,7 @@ export default function HomeCarousel() {
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp" className="w-full" />
+        <img src={Banner2} className="w-full object-cover" alt="Banner 2" />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a href="#slide1" className="btn btn-circle">
             ❮
@@ -24,7 +28,7 @@ export default function HomeCarousel() {
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
-        <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp" className="w-full" />
+        <img src={Banner3} className="w-full object-cover" alt="Banner 3" />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a href="#slide2" className="btn btn-circle">
             ❮
@@ -35,7 +39,7 @@ export default function HomeCarousel() {
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
-        <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp" className="w-full" />
+        <img src={Banner1} className="w-full object-cover" alt="Banner 4" />
         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
           <a href="#slide3" className="btn btn-circle">
             ❮
